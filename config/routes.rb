eb87f6f02e8925df39bc72a9c7ea0a1daef75f35
Josh Larson <joshua.larson@getbraintree.com>
2016-09-13 08:23:17 -0500

    Restrict /player routes to only :index and :create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  resources :players
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :players, :only => [:index, :create]
 end
